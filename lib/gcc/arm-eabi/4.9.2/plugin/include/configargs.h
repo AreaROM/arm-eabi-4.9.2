4a83fbacdd969bfd8ea8a926f25020718bc46101
Anthony Cox <motorhead1991@hotmail.com>
2015-04-29 18:28:12 -0600

    Initial commit

---
+++ lib/gcc/arm-eabi/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --prefix=/home/motorhead/toolchain/arm-eabi-4.9.2 --target=arm-eabi --enable-languages=c,c++ --disable-werror --disable-nls --disable-tls --disable-bootstrap --disable-libgomp --disable-shared --disable-libssp --disable-libquadmath --enable-threads --enable-target-optspace --disable-libatomic --with-gnu-as--with-gnu-ld --disable-libstdc++-v3 --with-gcc-version=GNU --with-pkgversion=Desolation_gnu_4.9.2-graphite";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
